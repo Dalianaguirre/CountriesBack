2792020b8d79b1289e6b7fc5bbfaf3a2b26726c0
Dalianaguirre <danathap@gmail.com>
2023-08-03 12:57:56 -0500

    Se dejó solo back

--- client/src/redux/store.js
+++
@@ -1,7 +0,0 @@
-import { createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
-import rootReducer from "./reducer";
-
-export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
-// thunk nos permite trabajar con asincronismo en el front
